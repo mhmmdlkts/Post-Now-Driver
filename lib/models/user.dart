5748b31cb325e979c91be0c5a4abf6a69e2da278
Muhammed Ali Aktaş <mhmmdlkts@gmail.com>
2020-10-18 07:12:49 +0200

    User credit self writeable bug fixed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -7,19 +7,14 @@ class User {
   String email;
   String languageCode;
   String token;
-  double credit;
 
-  User({this.name, this.phone, this.token, this.email, this.credit, this.languageCode});
+  User({this.name, this.phone, this.token, this.email, this.languageCode});
 
   User.fromSnapshot(DataSnapshot snapshot) {
     key = snapshot.key;
     name = snapshot.value["name"];
     phone = snapshot.value["phone"];
     email = snapshot.value["email"];
-    credit = snapshot.value["credit"];
-    if (credit == null)
-      credit = 0.0;
-    else credit += 0.0;
     token = snapshot.value["token"];
     languageCode = snapshot.value["lang"];
   }
@@ -28,7 +23,6 @@ class User {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     if (name != null) data['name'] = this.name;
     if (phone != null) data['phone'] = this.phone;
-    if (credit != null) data['credit'] = this.credit;
     if (email != null) data['email'] = this.email;
     if (token != null) data['token'] = this.token;
     if (languageCode != null) data['lang'] = this.languageCode;
